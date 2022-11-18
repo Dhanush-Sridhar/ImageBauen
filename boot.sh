20e10619a6117491fe9cdaa8e3d8545fb6c44aaf
Dhanush Sridhar <114331552+Dhanush-Sridhar@users.noreply.github.com>
2022-11-18 19:34:08 +0100

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+rootfs=$1
+dist="stretch"
+mirror="http://archive.raspbian.org/raspbian"
+include="net-tools,isc-dhcp-client,nano,openssh-server,rsync,wget"
+
+aptsources="deb http://archive.raspbian.org/raspbian stretch main contrib non-free\ndeb-src http://archive.raspbian.org/raspbian stretch main contrib non-free"
+
+if [ "$rootfs" == "" ]; then
+	echo "No directory to install to given."
+	exit 1
+fi
+
+if [ $EUID -ne 0 ]; then
+	echo "This tool must be run as root."
+	exit 1
+fi
+
+echo "Executing debootstrap..."
+debootstrap --arch armhf --foreign --variant minbase --no-check-gpg --include $include $dist $rootfs $mirror
+
+echo "Preparing for ARM emulation..."
+cp /usr/bin/qemu-arm-static $rootfs/usr/bin
+
+echo "Executing second stage debootstrap..."
+chroot $rootfs /debootstrap/debootstrap --second-stage
+
+echo "Configuring RootFS..."
+echo -e $aptsources > $rootfs/etc/apt/sources.list
+
+cat > $rootfs/etc/apt/apt.conf << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
+
+cat > $rootfs/etc/network/interfaces << EOF
+auto lo
+iface lo inet loopback
+auto eth0
+iface eth0 inet dhcp
+EOF
+
+echo "RootFS installation into '$rootfs' completed."
